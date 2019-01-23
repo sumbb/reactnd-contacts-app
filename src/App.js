c0a9380606a3d7d8c5d1e6dbc73fbee842ca9f9d
Sumedha Birajdar <birajdar@sumedhas-mbp.corp.adobe.com>
2019-01-23 13:20:34 +0530

    ListContacts component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import ListConstacts from './ListContacts';
 
+
+const contacts = [
+  {
+    "id": "karen",
+    "name": "Karen Isgrigg",
+    "handle": "karen_isgrigg",
+    "avatarURL": "http://localhost:5001/karen.jpg"
+  },
+  {
+    "id": "richard",
+    "name": "Richard Kalehoff",
+    "handle": "richardkalehoff",
+    "avatarURL": "http://localhost:5001/richard.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "handle": "tylermcginnis",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+ ];
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello World
-      </div>
+      <ListConstacts contacts= {contacts}/>
     );
   }
 }
---
+++ src/ListContacts.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class ListConstacts extends Component {
+    
+    render() {
+        console.log('props', this.props);
+        return 
+        <ol class='contact-list'>
+            
+        </ol>
+    }
+}
+
+export default ListConstacts;

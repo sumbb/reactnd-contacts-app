cd2b0a84f7668fe0c5d84651cfa28a2b01fb1802
Sumedha Birajdar <birajdar@sumedhas-mbp.corp.adobe.com>
2019-01-24 13:09:51 +0530

    Added state to the component

--- src/App.js
+++ src/App.js
@@ -2,30 +2,35 @@ import React, { Component } from 'react';
 import ListConstacts from './ListContacts';
 
 
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
- ];
+
 class App extends Component {
+  
+  state = {
+    contacts : [
+      {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+     ]
+  }
+
   render() {
     return (
-      <ListConstacts contacts= {contacts}/>
+      <ListConstacts contacts= {this.state.contacts}/>
     );
   }
 }
